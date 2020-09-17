01de09c46fe55fc9587fc4cd774d4296df815dd8
littleLane <lzz857183384@163.com>
2020-09-18 00:17:23 +0800

    feat: 110 自底向上的递归

--- src/algorithms/BinarySearchTree/110. 平衡二叉树.ts
+++ src/algorithms/BinarySearchTree/110. 平衡二叉树.ts
@@ -57,3 +57,24 @@ function isBalanced2(root: TreeNode | null): boolean {
 
   return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced2(root.left) && isBalanced2(root.right)
 }
+
+/**
+ * 自底向上的递归
+ * @param root
+ */
+function isBalanced3(root: TreeNode | null): boolean {
+  const height = (root: TreeNode | null): number => {
+    if (!root) return 0
+
+    const leftH = height(root.left)
+    const rightH = height(root.right)
+
+    if (leftH === -1 || rightH === -1 || Math.abs(leftH - rightH) > 1) {
+      return -1
+    }
+
+    return Math.max(leftH, rightH) + 1
+  }
+
+  return height(root) >= 0
+}
